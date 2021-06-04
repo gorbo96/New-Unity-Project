eea402044c07b719a9125753c267503e94bb77a9
gorbo96 <46572873+gorbo96@users.noreply.github.com>
2021-06-04 11:07:32 -0500

    mensaje para el commit

---
+++ Assets/Scripts/scranmInicio.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scranmInicio : MonoBehaviour
+{
+    public GameObject cnvGrupo;
+    public GameObject cnvGrupo1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void animacionStart(){        
+        if(cnvGrupo!=null){
+            Animator animator = cnvGrupo.GetComponent<Animator>();
+            bool animacion= animator.GetBool("boolAnimacion");
+            animator.SetBool("boolAnimacion",!animacion);
+        }
+        if(cnvGrupo1!=null){
+            Animator animator = cnvGrupo1.GetComponent<Animator>();
+            bool animacion= animator.GetBool("boolAnimacion");
+            animator.SetBool("boolAnimacion",!animacion);
+        }
+    }
+}
