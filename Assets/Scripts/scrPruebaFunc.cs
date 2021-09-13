7123d91da9814278db9165124c4a824e8793b98b
gorbo96 <46572873+gorbo96@users.noreply.github.com>
2021-09-13 16:16:53 -0500

    añadir paneles menu pricipal

--- Assets/Scripts/scrPruebaFunc.cs
+++ Assets/Scripts/scrPruebaFunc.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60352b5228930c598d4c0c502fa59f9775f1531ec0517b4ddf0c3b489a2c038d
-size 7738
+oid sha256:63251d79b89549efd6da8ac2d1da77d46b7b4536b8ceb2dc7e918ca8e26e5d9f
+size 7736
