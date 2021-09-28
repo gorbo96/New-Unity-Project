d28aad73a45f9cf8783361f0f03e97b514401d96
gorbo96 <46572873+gorbo96@users.noreply.github.com>
2021-09-28 11:01:04 -0500

    menu principal finalizado

--- Assets/Scripts/scrSeleccion.cs
+++ Assets/Scripts/scrSeleccion.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9255fcf219042e807fcee5a7902679a23d5a0496640bf8c5aae6523ab2b9bed
-size 9325
+oid sha256:52a868f723062bc0f3133d8ab6e9cf65aefb9e897b98f45fbf3de3c8778a09b3
+size 10990
