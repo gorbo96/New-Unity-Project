6eecf7646e37766b7efce5e8935a763d079bc52e
gorbo96 <46572873+gorbo96@users.noreply.github.com>
2021-10-11 17:20:59 -0500

    avances escenario accesibilidad

--- Assets/Scripts/scrPruebaFunc.cs
+++ Assets/Scripts/scrPruebaFunc.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63251d79b89549efd6da8ac2d1da77d46b7b4536b8ceb2dc7e918ca8e26e5d9f
-size 7736
+oid sha256:c17a73d862a3f7bed93da4e0d636ec11cc71eb1fe4253e5d6bae1cc46d3b34c8
+size 8522
--- Assets/Scripts/scrSeleccion.cs
+++ Assets/Scripts/scrSeleccion.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26ab1bb34040a5da5bc96284b281d03399a993a9d599fc678d0d445b309377da
-size 16792
+oid sha256:828538f705aa4425d9c9efda108563a791521926fadc183f965678bcdf2d2deb
+size 16788
