8acd7e38d6218852993efd2601251ee3fd3b62d2
gorbo96 <46572873+gorbo96@users.noreply.github.com>
2021-10-13 17:31:19 -0500

    cambios presentacion

--- Assets/Scripts/scrSeleccionNormal.cs
+++ Assets/Scripts/scrSeleccionNormal.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4766a9c98acb1e4753689de4d1d898d78f3977438abcb7dd2ee04f90bd16e37c
-size 6681
+oid sha256:4424abbce5312734a3afebbac5eecdcb410e6621c19927807e72a6f75e468764
+size 6858
