a6d7f376ca97233155b29b50fed18ed28a65bbe2
gorbo96 <46572873+gorbo96@users.noreply.github.com>
2021-10-13 16:16:02 -0500

    uso de raton escenario y menu terminado

--- Assets/Scripts/scrSeleccionNormal.cs
+++ Assets/Scripts/scrSeleccionNormal.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4aadaeb033b0d07544d5956ae6282c4597898d71f95fb999439eb18166a3e66b
-size 3471
+oid sha256:0b18fda47059b4717af2254054cbc5fc1f3fe7273df3db4eb3171113748099d5
+size 6406
