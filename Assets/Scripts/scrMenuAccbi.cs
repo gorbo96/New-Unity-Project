a31642a110d35f61e5b794fb16545c97a07ee46f
gorbo96 <46572873+gorbo96@users.noreply.github.com>
2021-12-10 16:47:07 -0500

    cambios fuentes

--- Assets/Scripts/scrMenuAccbi.cs
+++ Assets/Scripts/scrMenuAccbi.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8591ee9ec44e575a590f03c0b29c6bda63348fc2c0c7f1c5459f0e928dc2703c
-size 317
+oid sha256:6288be6452f6c1990e0609bd02310587da99be761d262f9b5c4b3a422efb447b
+size 528
