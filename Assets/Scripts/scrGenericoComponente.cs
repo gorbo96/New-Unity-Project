4be284c348d7c0c224b77fe952c32537655ee3b7
gorbo96 <46572873+gorbo96@users.noreply.github.com>
2021-12-20 17:22:43 -0500

    ultimos cambios

---
+++ Assets/Scripts/scrGenericoComponente.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e0c625d27850fec5de63166b8b2787717916a9312d7e4aa90e3b5012aeadbc75
+size 502
--- Assets/Scripts/scrPruebaFunc.cs
+++ Assets/Scripts/scrPruebaFunc.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b05d96f4801829dd4e8d1a020d7600d99256503c9b48f1689afe2965b6111c1
-size 12964
+oid sha256:cfbf6412fdb046ffd05a42c421dceca9a116413f4b1c7dd32a1b0bb2efd728f1
+size 13022
